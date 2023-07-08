9977b0f969531f04d003d9a95f296753fa5af236
Ghassan <ghassanelgendyy@gmail.com>
2023-07-08 16:58:11 +0300

    Update script.js
    Added the main functions to the game

---
+++ script.js
@@ -0,0 +1,47 @@
+function getComputerChoice() {
+	let randomNumber = Math.floor(Math.random() * 3);
+	switch (randomNumber) {
+		case 0: {
+			return "rock";
+			break;
+		}
+		case 1: {
+			return "paper";
+			break;
+		}
+		case 2: {
+			return "scissor";
+		}
+	}
+}
+function roundPlay(playerSelection, computerSelection) {
+	if (typeof playerSelection != "string") {
+		console.log("Please enter a valid choice!");
+	} else {
+		if (computerSelection === playerSelection) {
+			console.log("It's a TIE");
+		} else if (
+			(computerSelection === "rock" && playerSelection === "paper") ||
+			(computerSelection === "paper" && playerSelection === "scissor") ||
+			(computerSelection === "scissor" && playerSelection === "rock")
+		) {
+			console.log("You WIN");
+		} else {
+			console.log("Your LOSE");
+		}
+	}
+}
+function currentMove(playerSelection, computerSelection) {
+	console.log(
+		`You played ${playerSelection} the computer played ${computerSelection} `
+	);
+}
+
+const rounds = prompt("How many rounds do you want to play?");
+
+for (let i = 0; i < rounds; i++) {
+	let playerSelection = prompt("Enter your choice!").toLowerCase();
+	let computerSelection = getComputerChoice();
+	roundPlay(playerSelection, computerSelection);
+	currentMove(playerSelection, computerSelection);
+}
