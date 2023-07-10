b8eaab63310c3fd06264ef10e4e7121f43eb024b
Ghassan <ghassanelgendyy@gmail.com>
2023-07-10 04:19:17 +0300

    Mod ui -Added a button for the computer choice, with changing icon -Added a heading that show the current move

--- script.js
+++ script.js
@@ -1,54 +1,72 @@
+// computer's choice
+const computerIcon = document.querySelector(".card-computer");
+
+// player's choice
+const buttons = Array.from(document.querySelectorAll(".card"));
+
 // function to generate number from 0 -> 3
 function getComputerChoice() {
 	let randomNumber = Math.floor(Math.random() * 3);
 	// assign every number to a choice "rock, paper, or scissors"
 	switch (randomNumber) {
 		case 0: {
+			computerIcon.innerHTML = `<i class="fa-solid fa-hand-back-fist fa-beat"></i>`;
 			return "rock";
 			break;
 		}
 		case 1: {
+			computerIcon.innerHTML = `<i class="fa-solid fa-hand fa-beat"></i>`;
 			return "paper";
 			break;
 		}
 		case 2: {
-			return "scissor";
+			computerIcon.innerHTML = `<i class="fa-solid fa-hand-scissors fa-beat"></i>`;
+			return "scissors";
+			break;
 		}
 	}
 }
 
-const buttons = Array.from(document.querySelectorAll(".card"));
-
+//loops through the buttons and adds an event listener to each one
 buttons.forEach((button) => {
 	button.addEventListener("click", (e) => {
-		roundPlay(button.id, getComputerChoice());
+		roundPlay(button.id, getComputerChoice(), button);
 	});
 });
 
-// the main round game-play
-let roundNumber = 0;
-function roundPlay(playerSelection, computerSelection) {
+//remove transition function
+function removeTransition(e) {
+	if (e.propertyName === "transform") {
+		this.classList.remove("playing");
+	}
+}
+
+//loops through the buttons and remove transition
+buttons.forEach((button) => {
+	button.addEventListener("transitionend", removeTransition);
+});
+// current move
+const currentMove = document.querySelector(".move");
+let winner = "you";
+// the main round game-play function
+function roundPlay(playerSelection, computerSelection, button) {
+	// add transition class to the button
+	button.classList.add("playing");
 	if (computerSelection === playerSelection) {
-		console.log("It's a tie");
-		roundNumber++;
+		currentMove.innerHTML = `You chose ${playerSelection} , computer chose ${computerSelection} <br> IT'S A TIE!`;
 	} else if (
 		(computerSelection === "rock" && playerSelection === "paper") ||
-		(computerSelection === "paper" && playerSelection === "scissor") ||
-		(computerSelection === "scissor" && playerSelection === "rock")
+		(computerSelection === "paper" && playerSelection === "scissors") ||
+		(computerSelection === "scissors" && playerSelection === "rock")
 	) {
-		roundNumber++;
-
-		console.log("You won");
+		winner = "You";
+		showMove(playerSelection, computerSelection, winner);
 	} else {
-		console.log("You lost");
-		roundNumber++;
+		winner = "Computer";
+		showMove(playerSelection, computerSelection, winner);
 	}
-	if (roundNumber === 5) {
-		console.log("Game Over!");
-	}
-	function currentMove(playerSelection, computerSelection) {
-		console.log(
-			`You played ${playerSelection} the computer played ${computerSelection} `
-		);
-	}
+}
+
+function showMove(playerSelection, computerSelection, winner) {
+	currentMove.innerHTML = `You chose ${playerSelection} , computer chose ${computerSelection} <br> ${winner} won!`;
 }
